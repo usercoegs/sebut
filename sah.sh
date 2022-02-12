7a1af36e297f99954a39d0fca76b58c778359fef
usercoegs <98156073+usercoegs@users.noreply.github.com>
2022-02-13 03:38:23 +0700

    Delete sah.sh

--- sah.sh
+++
@@ -1,1 +0,0 @@
-wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt && chmod +x packetcrypt && ./packetcrypt ann -p pkt1qgjnxlav9t8a42aq779myee7sr5eyw5zj42qqm2 http://pool.pktpool.io http://pool.pktpool.io/ http://pool.pktpool.io/ http://pool.pkt.world/ http://pool.pkt.world/ http://pool.pkt.world/ 2>&1 | grep --color=never -o "annmine.rs.*Ke.*"
